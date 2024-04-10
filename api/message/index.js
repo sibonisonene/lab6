f40570e36157eb9d69728fbfd10e65580a2e00ec
Siboniso Nene <sibonisodev@gmail.com>
2024-04-10 21:49:57 +0200

    Rearrange files in the directory

--- api/message/index.js
+++ api/message/index.js
@@ -1,13 +1,52 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+//create cars api using express
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3001;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+app.use(bodyParser.json()); //epress to body-parser
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+const cars = require('./cars.json');
+
+//get all cars
+app.get('/cars', (req, res) => {
+    res.json(cars);
+});
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars.find(car => car.id === id);
+    res.json(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[index] = updatedCar;
+    res.json(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+    console.log(req);
+    const newCar = req.body;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+//start app at localhost:3001
+app.listen(3001, () => {
+    console.log('Server started at http://localhost:3001');
+});
